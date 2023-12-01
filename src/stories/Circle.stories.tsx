3b51d5ba15d54b6f9dcb27c2f1ddce0e8d6ec491
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-12-01 12:45:03 +0900

    ✨storybookカタログのドキュメント作成

--- src/stories/Circle.stories.tsx
+++ src/stories/Circle.stories.tsx
@@ -8,29 +8,42 @@ const meta = {
   argTypes: {
     variant: { control: 'select', options: ['orange', 'green', 'yellow'] },
   },
+  tags: ['autodocs'],
 } satisfies Meta<typeof Circle>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
+/**
+ * オレンジ色の円です
+ */
 export const BaseCircle: Story = {
   args: {
     variant: 'orange',
   },
 }
 
+/**
+ * 緑色の円です
+ */
 export const GreenCircle: Story = {
   args: {
     variant: 'green',
   },
 }
 
+/**
+ * 黄色の円です
+ */
 export const YellowCircle: Story = {
   args: {
     variant: 'yellow',
   },
 }
 
+/**
+ * 円の一覧です
+ */
 export const GroupCircle: Story = {
   render: () => (
     <div className="p-10">

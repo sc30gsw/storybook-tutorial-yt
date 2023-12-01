bfaf5ae0adf1d1d6e06dfbca84b39800ae4300bb
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-12-01 11:37:37 +0900

    ✨storybookのパスエイリアスの設定

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -16,5 +17,13 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve = config.resolve || {}
+    config.resolve.alias = {
+      ...config.resolve?.alias,
+      "@": path.resolve(__dirname, "../src"),
+    };
+    return config;
+  }
 };
 export default config;
